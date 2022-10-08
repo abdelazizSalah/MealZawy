70cc34e8ce729eda7e3b2d93648b2f09634673c8
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-10-08 21:53:35 +0200

    styling the app bar

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.deepPurple[900],
+              centerTitle: true,
+              elevation: 10,
+              titleTextStyle: TextStyle(
+                  color: Colors.orange[300],
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  shadows: [
+                    Shadow(
+                        blurRadius: 100,
+                        color: Colors.amber,
+                        offset: Offset(20, 10))
+                  ]))),
       debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
@@ -24,10 +39,16 @@ class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctx) {
     return Scaffold(
-      appBar: AppBar(),
       drawer: Drawer(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.food_bank_rounded),
+          onPressed: () => Scaffold.of(ctx).openDrawer(),
+        ),
+        title: Text("MealZawy"),
+      ),
       body: Container(
         child: Text('Hello World!'),
       ),
