f797039ac411f8c58269ac26034b745503a63724
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-10-15 10:17:58 +0200

    Selecting meals depending on the category

--- lib/Screens/category_meal_screen.dart
+++ lib/Screens/category_meal_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../Data/dummy_data.dart';
+import '../Models/meal.dart';
 
 class CategoryMealScreen extends StatelessWidget {
   @override
@@ -7,11 +9,31 @@ class CategoryMealScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final String catTitle = Args['title'] as String;
     final catID = Args['id'];
+
+    /// we need to make a list filtered by the id
+    /// that each category contains only the meals that has its id
+
+    /// explaination: ana 3mlt list mn el meals
+    /// 2- hya 3bara 3n subset mn el dummyMeals
+    /// 3- m722a condition el hwa
+    /// 4- en el categories bta3t el meal el hattl3 lazm tkon nfs
+    /// el category bta3 el meal ena ana da5el 3leha
+    final List<Meal> mealsList = dummyMeals.where((meal) {
+      return meal.categories.contains(catID);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(catTitle),
       ),
-      body: const Center(child: Text('The Recipes For the Categories')),
+      body: Center(
+        child: Column(
+          children: [
+            ...mealsList.map((meal) {
+              return Text(meal.title);
+            }).toList()
+          ],
+        ),
+      ),
     );
   }
 }
